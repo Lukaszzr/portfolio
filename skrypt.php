5c283d1bbaced5717b38425da3d667b3c9f9f709
Lukasz Rozdzynski <lukas783@wp.pl>
2020-01-21 21:52:12 +0100

    add-file

--- skrypt.php
+++ skrypt.php
@@ -1,23 +1,12 @@
+
 <?php
-$header  = "From: https://lukaszzr.github.io/portfolio/. \r\n";
-$header .= 'MIME-Version: 1.0'."\r\n";
-$header .= 'Content-type: text/html; charset=UTF-8'."\r\n";
 $to      = 'lukaszrozdzynskii@gmail.com';
-$subject = 'Wiadomość ze strony https://lukaszzr.github.io/portfolio/.';
-$from = $_POST['email'];
 $name = $_POST['name'];
-$email=$_POST['email'];
-$emailbody = 
-	'Imie: '.$_POST['name']. "\n"
-	.' Adres mailowy: '.$_POST['email']. "\n"
-	.'Wiadomosc: '.$_POST['message']. "\n";
+$email = $_POST['email'];
+$subject = 'Nowy e-mail od' . $name . '(' . $email . ')';
+$message = $_POST['message'];
+$headers  = 'From: ' . $name . '(' . $email . ')';
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-
-   if(mail($to, $subject, $emailbody, $header)) 
-
-
-{
-   
-   echo "<script> window.location = \"index.html\" </script>";
-}
-?>
+mail($to, $subject, $message, $headers);
+?> 
