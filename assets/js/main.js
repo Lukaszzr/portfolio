9bc9acc9020025f98dc221b9b98940f93d056ceb
Lukasz Rozdzynski <lukas783@wp.pl>
2020-01-22 21:02:09 +0100

    add portfolio-files

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,17 @@
+//mobile navigation
 $(function() {
   $(".mobile-button").click(function() {
     $(".menu").toggleClass("menu--show");
   });
 });
-
+//add background-color to navbar link
+$(".menu__link").click(function(e) {
+  $(".menu__link").removeClass("menu__link--active");
+  $(this).addClass("menu__link--active");
+  e.preventDefault();
+});
+//scroll to section click menu-link
 var linkScroll = $(".scroll");
-
 linkScroll.click(function(e) {
   e.preventDefault();
   $("body, html").animate(
